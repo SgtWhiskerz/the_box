556f1ca399a285af2f334b58fd5cd7b1d10e1e74
SgtWhiskerz <sgtwhiskerz@gmail.com>
2024-02-08 16:55:11 -0700

    Add options for negative values (not sold on using longs for returns yet)

--- Helpers.h
+++ Helpers.h
@@ -3,12 +3,24 @@
 
 #include "FastLED.h"
 
-constexpr int getSec(unsigned long millis) { return (millis / 1000) % 60; }
-constexpr int getMin(unsigned long millis) { return millis / 60000; }
+/**
+ * Take milliseconds and return seconds as if displayed on a clock (i.e. DOES
+ * NOT convert the whole value, just the remainder)
+ */
+constexpr unsigned long getSec(unsigned long millis) {
+  return (millis / 1000) % 60;
+}
+constexpr long getSec(long millis) { return (millis / 1000) % 60; }
+constexpr unsigned long getMin(unsigned long millis) { return millis / 60000; }
+constexpr long getMin(long millis) { return millis / 60000; }
 
-constexpr unsigned long operator""_min(unsigned long long min) { return min * 60000; }
+constexpr unsigned long operator""_min(unsigned long long min) {
+  return min * 60000;
+}
 constexpr long double operator""_min(long double min) { return min * 60000; }
-constexpr unsigned long long operator""_sec(unsigned long long sec) { return sec * 1000; }
+constexpr unsigned long long operator""_sec(unsigned long long sec) {
+  return sec * 1000;
+}
 constexpr long double operator""_sec(long double sec) { return sec * 1000; }
 
 void displayColor(CRGB color);
